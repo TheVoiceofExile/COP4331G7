0fca48e6ff1ff7eb54c59175a9d31031a935a25a
Cory Williamson <zekos@ymail.com>
2017-06-02 21:00:37 -0400

    Basic test room and movement
    I used the floor template for the room and main character draft 1 for
    the character to get some basic wasd movement. I'm not sure why the
    camera won't show all of the floor template even though it's resolution
    is set to 240x160.

---
+++ COP4331G7/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 1f;
+	
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		body.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * speed, 0.8f),
+		                            Mathf.Lerp(0, Input.GetAxis("Vertical") * speed, 0.8f));
+	}
+}
