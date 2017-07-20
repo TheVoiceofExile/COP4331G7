b1ad696a3cfbc59a1f27d9f7fecf1f4b90738a4c
Hanser <hanserg45@knights.ucf.edu>
2017-07-20 14:37:50 -0400

    Try again

--- 4331 COPPER LN/Assets/Scripts/PlayerController.cs
+++ 4331 COPPER LN/Assets/Scripts/PlayerController.cs
@@ -5,36 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
-	public CameraMovement cameraMovement;
 
     private Animator anim;
     private Rigidbody2D myRigidBody;
-	private Vector2 lastMove;
 
-	private static bool playerExists;
     private bool playerMoving;
+    private Vector2 lastMove;
 
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
-
-		if (!playerExists)
-		{
-			playerExists = true;
-			DontDestroyOnLoad(transform.gameObject);
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
     }
 
     // Update is called once per frame
     void Update()
     {
-
         playerMoving = false;
 
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
@@ -68,49 +55,5 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
-
-		float horizontalMove = Input.GetAxisRaw("Horizontal");
-		float vericalMove = Input.GetAxisRaw("Vertical");
-		playerMoving = false;
-
-		if (cameraMovement.moving)
-		{
-			myRigidBody.velocity = new Vector2(0, 0);
-			anim.SetFloat("Horizontal", 0);
-			anim.SetFloat("Vertical", 0);
-		}
-		else
-		{
-			if (Mathf.Abs(horizontalMove) > 0.5f)
-	        {
-				myRigidBody.velocity = new Vector2(horizontalMove * speed, myRigidBody.velocity.y);
-	            playerMoving = true;
-				lastMove = new Vector2(horizontalMove, 0);
-	        }
-
-			if (Mathf.Abs(vericalMove) > 0.5f)
-	        {
-				myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, vericalMove * speed);
-	            playerMoving = true;
-				lastMove = new Vector2(0, vericalMove);
-	        }
-
-			if(Mathf.Abs(horizontalMove) < 0.5f)
-	        {
-	            myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
-	        }
-
-			if (Mathf.Abs(vericalMove) < 0.5f)
-	        {
-	            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f);
-	        }
-
-			anim.SetFloat("MoveX", horizontalMove);
-			anim.SetFloat("MoveY", vericalMove);
-	        anim.SetBool("PlayerMoving", playerMoving);
-	        anim.SetFloat("LastMoveX", lastMove.x);
-	        anim.SetFloat("LastMoveY", lastMove.y);
-		}
-
     }
 }
