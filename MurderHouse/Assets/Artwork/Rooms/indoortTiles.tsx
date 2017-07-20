95ab7d131dde78060dbc769c9ae749d774911f5b
Cory <zekos@ymail.com>
2017-07-20 11:24:34 -0400

    fixing folders

--- MurderHouse/Assets/Artwork/Rooms/indoortTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1680" columns="56">
- <image source="../../../../pixelArt/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
-</tileset>
