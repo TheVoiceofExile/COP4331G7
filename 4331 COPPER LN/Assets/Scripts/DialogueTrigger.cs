0271296d9a58ef1a1a1c0850f34d62aee0dfe235
Dylan Clemann <dev@clemann.com>
2017-07-21 19:59:27 -0400

    Dialogues

---
+++ 4331 COPPER LN/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour 
+{
+	public GameObject textBox;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			other.GetComponent<PlayerController> ().canMove = false;
+			Debug.Log("Triggered");
+			textBox.GetComponent<TextBoxManager> ().EnableTextBox ();
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ 4331 COPPER LN/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour 
+{
+	private bool isActive;
+
+	public PlayerController player;
+
+	void Start ()
+	{
+		isActive = false;
+		gameObject.SetActive(false);
+	}
+		
+	void Update ()
+	{
+		if(isActive)
+		{
+			if(Input.GetKey(KeyCode.Space))
+			{
+				Debug.Log("Space");
+				DisableTextBox ();
+			}
+		}
+	}
+
+	public void EnableTextBox()
+	{
+		Debug.Log("Enabled");
+		gameObject.SetActive(true);
+		isActive = true;
+
+	}
+
+	public void DisableTextBox()
+	{
+		Debug.Log("Disabled");
+		gameObject.SetActive(false);
+		isActive = false;
+		player.canMove = true;
+		Destroy (gameObject);
+	}
+
+}
